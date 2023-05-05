441e4e1eb4b9d88a63d5b0d2210c5c4ca92262c6
josan14130716 <santos.jose00014@gmail.com>
2023-05-04 17:53:40 -0700

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Feature(models.Model):
+    
+    name = models.CharField(max_length=90, default='')
+    details = models.CharField(max_length=90, default='')
+    
+    
